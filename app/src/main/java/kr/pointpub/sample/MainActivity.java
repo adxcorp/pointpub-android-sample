ff8773b1ee15d97d3588d0378ae675d46705b378
YongSun Kim <sunny@adxcorp.kr>
2024-09-11 18:28:46 +0900

    [수정] 테스트용 App ID 교체 [수정] 포인트펍 SDK 0.0.3 업데이트 [수정] 로그 활성화 추가

--- app/src/main/java/kr/pointpub/sample/MainActivity.java
+++ app/src/main/java/kr/pointpub/sample/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import kr.pointpub.sdk.feature.config.PointPub;
+import kr.pointpub.sdk.PointPub;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (BuildConfig.DEBUG) {
+            // 디버깅을 위한 로그 활성화
+            PointPub.INSTANCE.enableLogTrace();
+        }
         findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText editText = findViewById(R.id.et_uid);
                 String uid = editText.getText().toString();
                 if (!TextUtils.isEmpty(uid)) {
+                    // 포인트펍 실행 (user id값 필수 입력)
                     PointPub.INSTANCE.startOfferWall(MainActivity.this, uid);
                 } else {
+                    // 포인트펍 실행 (test user)
                     PointPub.INSTANCE.startOfferWall(MainActivity.this, "123456789");
                 }
             }
